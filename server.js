9a568028741149711734c51571f56f7c82d66342
Jihad Bunkheila <jihadzackaria@gmail.com>
2021-09-19 06:27:50 -0400

    Fixes and formatting

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const CombinedUrl = require('./models/combinedUrl')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const CombinedUrl = require("./models/combinedUrl");
+const app = express();
+var cors = require("cors");
 
-mongoose.connect('mongodb://localhost/urlCombiner', {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+app.use(cors());
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+mongoose.connect("mongodb://localhost/urlCombiner", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.post('/urlCombiner', async (req, res) => {
-  const combinedUrl = await CombinedUrl.create({ full: req.body.fullUrls })
-  return res.status(200).send(combinedUrl.combined);
-})
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/:combinedUrl', async (req, res) => {
-  const combinedUrl = await CombinedUrl.findOne({ combined: req.params.combinedUrl })
-  if (combinedUrl == null) return res.sendStatus(404)
-    res.render('index', { combinedUrl: combinedUrl })
-})
+app.post("/urlCombiner", async (req, res) => {
+  const parsedUrls = req.body.fullUrls.split(",");
+  const combinedUrl = await CombinedUrl.create({
+    full: parsedUrls,
+  });
+  return res.status(200).json(combinedUrl.combined);
+});
+
+app.get("/:combinedUrl", async (req, res) => {
+  const combinedUrl = await CombinedUrl.findOne({
+    combined: req.params.combinedUrl,
+  });
+  if (combinedUrl == null) return res.sendStatus(404);
+  res.render("index", { combinedUrl: combinedUrl });
+});
 
 app.listen(process.env.PORT || 5000);
